bf862c19db47dfadda728ffe5aa041f277b796ce
Hugh Caluscusin (Dev) <hugh@melodiccrypter.com>
2020-03-11 10:01:10 +0800

    modified

--- src/util/shouldCompress.js
+++ src/util/shouldCompress.js
@@ -1,11 +1,8 @@
 import compression from 'compression';
 
 const shouldCompress = (req, res) => {
-    if (req.headers['x-no-compression']) {
-        // don't compress responses if this request header is present
-        return false;
-    }
-
+    // No compression
+    if (req.headers['x-no-compression']) return false;
     // fallback to standard compression
     return compression.filter(req, res);
 };
